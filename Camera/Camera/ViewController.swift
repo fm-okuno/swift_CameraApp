f8b22436799a66b5599e9f656777c4fbdecddfe6
gohta <okuno@freemake.co.jp>
2020-04-08 15:03:46 +0900

    xcodeにてプロジェクトを作成 プロジェクト名「Camera」

---
+++ Camera/Camera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by 開発アカウント on 2020/04/08.
+//  Copyright © 2020 開発アカウント. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
