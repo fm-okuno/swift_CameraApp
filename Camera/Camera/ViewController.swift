0419e7eb9c5a710370550baaf068b474ebc4a45b
gohta <okuno@freemake.co.jp>
2020-04-08 16:43:32 +0900

    Main.storyboard 　・画面上にimageView、toolbar、cameraボタンを配置し、各ボタンにレイアウトの設定を行った 　・各部品をViewController.swiftと接続
    ViewController.swift
    　・ViewControllerに下記の2つのプロトコルを批准させた
    　　→UIImagePickerControllDelegate（カメラや写真アルバムから画像を取得する操作のためのプロトコル）
    　　→UINavigationControllerDelegete（複数のビューコントローラーを操作する為に使用するプロトコル）
    　・画像の取得方法を定数cameraに代入
    　・カメラの無い端末でコードが実行されない様にチェック

--- Camera/Camera/ViewController.swift
+++ Camera/Camera/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//ViewControllerクラスに必要なプロトコルを批准させる
+class ViewController: UIViewController,UIImagePickerControllerDelegate,
+                                       UINavigationControllerDelegate
+{
 
+    //UIImageViewと接続
+    @IBOutlet private weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    //カメラボタンと接続
+    @IBAction private func launchCameraButton(_ sender: UIBarButtonItem) {
+        
+        //画像の取得方法を定数cameraに代入（カメラを起動し、撮影した写真を取得）
+        let camera = UIImagePickerController.SourceType.camera
+        
+        //カメラの無い端末でコードが実行されない様に確認
+        if UIImagePickerController.isSourceTypeAvailable(camera) {
+            let picker = UIImagePickerController()
+            picker.sourceType = camera
+            picker.delegate = self
+            self.present(picker, animated: true)
+        }
+    }
 }
 
